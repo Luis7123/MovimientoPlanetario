78c9a76e9f3192456ae4e32becb2337b7c7ae3e6
Luis <arenasvillamizar.luiscarlos@gmail.com>
2023-09-05 00:13:56 -0500

    changes

--- doc/conf.py
+++ doc/conf.py
@@ -40,14 +40,14 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = ['.rst','.md']
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
 
 # General information about the project.
-project = 'Movimiento Planetario'
+project = 'MovimientoPlanetario'
 copyright = '2023, Luis Carlos'
 author = 'Luis Carlos'
 
@@ -165,6 +165,3 @@ texinfo_documents = [
      author, 'WordCount', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
